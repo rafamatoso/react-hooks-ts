324b879ac592a6681c857b2aecb657e40f97f45a
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-01 17:29:31 -0300

    useState e useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <h1>App funcionando</h1>
-  );
+interface User {
+  name: 'string';
+  login: 'string';
+  avatarUrl: 'string';
 }
+
+const App: React.FC = () => {
+  const [user, setUser] = useState<User>();
+
+  async function loadData() {
+    const response = await fetch('https://api.github.com/users/rafamatoso');
+    const data = await response.json();
+
+    setUser(data);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [user]);
+
+  return (
+    <h1>{user?.name}</h1>
+  );
+};
 
 export default App;
